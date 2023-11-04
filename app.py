ca8a482f6f833364e797ea850322e8ad5f91802c
SayuryKato Kato <skdp.ecp19@uea.edu.br>
2023-11-04 05:35:11 +0000

    :tada: Áudio para texto

--- app.py
+++ app.py
@@ -1,25 +1,113 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
 import speech_recognition as sr
 from gtts import gTTS
+import pickle
 
 app = Flask(__name__)
+## carregando o modelo para o código
+with open("reconehcimentoVoz.pickle", "rb") as file:
+    modelo_carregado = pickle.load(file)
+
+# Função para pré-processar o espectrograma para o modelo
+def preprocess_spectrogram_for_model(spectrogram):
+    try:
+        # Redimensione o espectrograma para o tamanho esperado pelo modelo
+        input_size = (model_input_height, model_input_width)  # Substitua pelos valores reais
+        preprocessed_spectrogram = resize_spectrogram(spectrogram, input_size)
+
+        # Realize a normalização, se necessário
+        preprocessed_spectrogram = normalize_spectrogram(preprocessed_spectrogram)
+
+        return preprocessed_spectrogram
+    except Exception as e:
+        raise Exception(f"Erro ao pré-processar o espectrograma para o modelo: {str(e)}")
+
+# Função para redimensionar o espectrograma para o tamanho esperado
+import numpy as np
+import cv2  # Certifique-se de ter a biblioteca OpenCV instalada
+
+# Função para redimensionar o espectrograma para o tamanho esperado
+def resize_spectrogram(spectrogram, target_size):
+    try:
+        # Redimensione o espectrograma usando bibliotecas como OpenCV
+        # Implemente a lógica de redimensionamento aqui
+        # Certifique-se de redimensionar corretamente o espectrograma para o tamanho esperado pelo modelo
+        preprocessed_spectrogram = cv2.resize(spectrogram, target_size, interpolation=cv2.INTER_LINEAR)
+
+        return preprocessed_spectrogram
+    except Exception as e:
+        raise Exception(f"Erro ao redimensionar o espectrograma: {str(e)}")
+
+# Função para normalizar o espectrograma, se necessário
+# Função para normalizar o espectrograma, se necessário
+def normalize_spectrogram(spectrogram):
+    try:
+        # Implemente a lógica de normalização aqui
+        # Você pode realizar normalização min-max, z-score, ou outro método apropriado
+        # Certifique-se de que o espectrograma esteja normalizado no intervalo esperado pelo modelo
+
+        # Exemplo de normalização min-max:
+        min_value = np.min(spectrogram)
+        max_value = np.max(spectrogram)
+        normalized_spectrogram = (spectrogram - min_value) / (max_value - min_value)
+
+        return normalized_spectrogram
+    except Exception as e:
+        raise Exception(f"Erro ao normalizar o espectrograma: {str(e)}")
+
+def preprocess_audio(audio_data):
+    try:
+        # Converta o áudio em um espectrograma usando a biblioteca librosa
+        y, sr = librosa.load(io.BytesIO(audio_data), sr=None)
+        spectrogram = librosa.feature.melspectrogram(y, sr=sr)
+        # Realize o pré-processamento adicional, como normalização, redimensionamento, etc.
+        return spectrogram
+    except Exception as e:
+        raise Exception(f"Erro ao pré-processar o áudio: {str(e)}")
+
+
+
+# Função para fazer previsões com base no modelo carregado
+def model_predict(spectrogram):
+    try:
+        # Realize qualquer processamento adicional necessário para alimentar o espectrograma no modelo
+        # Suponha que você tenha um modelo carregado em 'modelo_carregado'
+        # Certifique-se de que o modelo esperado esteja configurado corretamente
+        # Por exemplo, converta o espectrograma para o formato de entrada esperado pelo modelo
+        input_data = preprocess_spectrogram_for_model(spectrogram)
+
+        # Faça previsões com base no modelo
+        predictions = modelo_carregado.predict(input_data)
+
+        # Retorne as previsões
+        return predictions
+    except Exception as e:
+        raise Exception(f"Erro ao fazer previsões: {str(e)}")
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        if "audio" in request.files:
+            try:
+                # Receba um arquivo de áudio do formulário
+                audio_file = request.files["audio"]
+
+                # Realize a conversão de áudio para texto usando o modelo
+                audio_data = audio_file.read()
+
+                # Faça o pré-processamento dos dados de áudio, converta para espectrograma, etc.
+                spectrogram = preprocess_audio(audio_data)
+
+                # Faça previsões usando o modelo carregado
+                predictions = model_predict(spectrogram)
+
+                return render_template('result2.html',transcript=predictions)
+            except Exception as e:
+                return jsonify({"error": str(e)})
+
     return render_template('index.html')
-
-def transcribe_audio(audio_file):
-    recognizer = sr.Recognizer()
-
-    with sr.AudioFile(audio_file) as source:
-        audio_data = recognizer.record(source)
-    try:
-        transcript = recognizer.recognize_google(audio_data)  # Usando o reconhecimento de fala do Google
-        return transcript
-    except sr.UnknownValueError:
-        return "Não foi possível reconhecer a fala"
-    except sr.RequestError as e:
-        return f"Erro ao se conectar ao serviço de reconhecimento de fala: {e}"
 
 @app.route('/result', methods=['POST'])
 def result():
@@ -37,66 +125,15 @@ def result():
 if __name__ == '__main__':
     app.run(debug=True)
 
-#teste
-# @app.route('/api/asr', methods=['POST'])
-# def asr():
-#     audio_data = request.files['audio']
-#     # Chame o modelo ASR para processar o áudio e obter o texto reconhecido
-#     recognized_text = process_audio_with_asr(audio_data)
-#     return jsonify({'text': recognized_text})
 
-# def process_audio_with_asr_model(audio_file):
-#     # Carregar o modelo ASR
-#     model = tf.saved_model.load("caminho_para_o_modelo_salvo")
-#     # Faça a inferência com o modelo ASR
-#     recognized_text = model.infer(audio_file)
-#     return recognized_text
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'audio_file' not in request.files:
-#         return "Nenhum arquivo de áudio enviado"
-
-#     audio_file = request.files['audio_file']
-    
-#     # Chame a função que processa o áudio com o modelo ASR
-#     recognized_text = process_audio_with_asr_model(audio_file)
-
-#     return render_template('result.html', result=recognized_text)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     
-
-#     return render_template('index.html')
-
+# transcribed_text = transcribe_audio(audio_file)
 # def transcribe_audio(audio_file):
-#     recognizer = sr.Recognizer()
-
-#     with sr.AudioFile(audio_file) as source:
-#         audio_data = recognizer.record(source)
-
 #     try:
 #         transcript = recognizer.recognize_google(audio_data)  # Usando o reconhecimento de fala do Google
+#         transcript = model
+
 #         return transcript
 #     except sr.UnknownValueError:
 #         return "Não foi possível reconhecer a fala"
 #     except sr.RequestError as e:
 #         return f"Erro ao se conectar ao serviço de reconhecimento de fala: {e}"
-
-
-
-    # if request.method == 'POST':
-    #     audio_file = request.files['audio']  # Obter o arquivo de áudio do formulário
-
-    #     if audio_file:
-    #         transcribed_text = transcribe_audio(audio_file)
-    #         return render_template('result.html', transcript=transcribed_text)
